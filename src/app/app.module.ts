4d1c3ac66d467f49de536791f753f3cdc1a35433
VShka <viktor.shkarupa@yandex.ru>
2020-09-02 14:27:00 +0300

    1) Добавил компонент кнопок. 2) Поправил шаблон и стили формы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
