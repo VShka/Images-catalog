2e3f1b21623dd5bf4781cb121089dc7d8499737a
VShka <viktor.shkarupa@yandex.ru>
2020-09-02 17:06:59 +0300

    Начал работу с кнопками. Добавил некоторый функционал переключения кнопки и примерные методы компонента для работы с формой и каталогом

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -9,7 +9,28 @@ export class ButtonComponent implements OnInit {
 
   constructor() { }
 
+  public btnState = {
+    group: 'Группировать',
+    ungroup: 'Разгруппировать'
+  };
+  public defaultBtnState = this.btnState.group;
+
   ngOnInit(): void {
+  }
+
+  public sendRequest(): any {
+    console.log(`It's working!`);
+  }
+
+  public deleteCatalog(): void {
+    console.log('Its working');
+  }
+
+  public toggleState(): string {
+    if (this.defaultBtnState === this.btnState.group) {
+      return this.defaultBtnState = this.btnState.ungroup;
+    }
+    return this.defaultBtnState = this.btnState.group;
   }
 
 }
