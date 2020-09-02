0f71c65a4a961ee59b65cbbe2546c5cfa95f8a62
VShka <viktor.shkarupa@yandex.ru>
2020-09-02 16:10:39 +0300

    Добавил компонент картинки

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { ButtonComponent } from './button/button.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    ButtonComponent
+    ButtonComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule
